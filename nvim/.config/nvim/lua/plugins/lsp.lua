81c4ef187c2703815b959e47efa9419e2e794eda
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2026-01-03 20:55:20 +0100

    python LSP'

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -183,6 +183,7 @@ return {
 			local servers = {
 				clangd = {},
 				rust_analyzer = {},
+				pyright = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
