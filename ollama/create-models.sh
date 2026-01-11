29c6c310fae0971f8fb8b909e336bcb2e1e545b0
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2026-01-11 22:34:49 +0100

    Add deepseek

--- ollama/create-models.sh
+++ ollama/create-models.sh
@@ -2,3 +2,6 @@
 ollama create qwen3:8b-32k-rust -f ./qwen3:8b-32k-rust
 ollama create qwen3:8b-32k-react -f ./qwen3:8b-32k-react
 ollama create qwen3:8b-32k-java-spring-boot -f ./qwen3:8b-32k-java-spring-boot
+ollama create deepseek-coder-v2:16b-rust -f ./qwen3:8b-32k-rust
+ollama create deepseek-coder-v2:16b-react -f ./qwen3:8b-32k-react
+ollama create deepseek-coder-v2:16b-java-spring-boot -f ./qwen3:8b-32k-java-spring-boot
