93a5d86bbff26525342befe33b798b72b4bbb11a
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2026-01-11 13:22:55 +0100

    Ollama

---
+++ ollama/create-models.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+ollama create qwen3:8b-32k-rust -f ./qwen3:8b-32k-rust
+ollama create qwen3:8b-32k-react -f ./qwen3:8b-32k-react
+ollama create qwen3:8b-32k-java-spring-boot -f ./qwen3:8b-32k-java-spring-boot
